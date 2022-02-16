e117bd428a152f1aba8e146bd112fcd7b315fcc4
Henrique Herbst Rosa <henriquehhr@gmail.com>
2022-02-16 01:04:02 -0300

    added quizzes list page

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+// GLOBAL VARIABLES
+let quizzes;
+
+// FUNCTIONS
+function getQuizzes() {
+  let promisse = axios.get(
+    "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
+  );
+  promisse.then(renderQuizzesPreview);
+}
+
+function renderQuizzesPreview(response) {
+  const quizzesUL = document.querySelector(".all-quizzes-list ul");
+  quizzes = response.data;
+  quizzes.forEach((element) => {
+    quizzesUL.innerHTML += `
+    <li class="quiz-preview" onclick="answerQuiz(${element.id})">
+            <img src=${element.image}/>
+            <div class="quiz-preview__linear-gradient">
+              <p>${element.title}
+              </p>
+            </div>
+          </li>`;
+  });
+}
+
+function answerQuiz(quiz) {
+  //teu cógigo aqui
+  console.log("A ID da quiz é: " + quiz);
+}
+
+getQuizzes();
